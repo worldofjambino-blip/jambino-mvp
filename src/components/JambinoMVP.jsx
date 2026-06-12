5e1edf2a6b9812f851e60286055044cd38b35ad9
worldofjambino-blip <worldofjambino@gmail.com>
2026-06-12 12:05:31 +0000

    feat: einheitliches Jambino-Pin Design

--- src/components/JambinoMVP.jsx
+++ src/components/JambinoMVP.jsx
@@ -210,12 +210,11 @@ export default function JambinoMVP() {
                 key={pg.id}
                 position={[pg.latitude, pg.longitude]}
                 icon={L.icon({
-                  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
-                  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-                  iconSize: [25, 41],
-                  iconAnchor: [12, 41],
-                  popupAnchor: [1, -34],
-                  shadowSize: [41, 41]
+                  iconUrl: '/jambino-pin.svg',
+                  
+                  iconSize: [40, 56],
+                  iconAnchor: [20, 56],
+                  popupAnchor: [0, -56],
                 })}
                 eventHandlers={{
                   click: () => setSelectedPlayground(pg),
